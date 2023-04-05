7dfd0e9ebdab5061e09171193eadf492e852ed2a
robzarel <robzarel@yandex.ru>
2023-04-05 14:20:00 +0200

    add favorite list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import Styles from './App.module.css';
 
 import useFdc3 from './hooks';
 
-type W = Window &
-  typeof globalThis & {
-    fdc3: any;
-  };
+type USER = {
+  userId: string;
+  username: string;
+  email: string;
+  avatar: string;
+  password: string;
+  birthdate: Date;
+  registeredAt: Date;
+  isFavorite: boolean;
+}
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [channel, setChannel] = useState<any>();
+  const [users, setUsers] = useState<USER[]>([]);
 
   const fdc3 = useFdc3();
 
@@ -21,19 +28,22 @@ function App() {
   },[fdc3])
 
   useEffect(() => {
-    if (fdc3) {
-      const listener = fdc3.addIntentListener('ViewContact', async (context: any) => {
-        console.log('context', context);
-        setCounter(context.id.count);
-      });
+    const counterListener = channel?.addContextListener('counter', (context: any) => {
+      setCounter(context.id.count);
+    });
+    const favoriteListener = channel?.addContextListener('favorite', (context: any) => {
+      setUsers(context.id.users);
+    });
 
-      return () => listener.unsubscribe();
+    return () => {
+      counterListener?.unsubscribe();
+      favoriteListener?.unsubscribe();
     }
-  },[fdc3])
+  },[channel]);
 
   const handleSendClick = () => {
     channel.broadcast({
-      type: 'fdc3.contact',
+      type: 'counter',
       id: { count: counter }
     });
   }; 
@@ -42,13 +52,28 @@ function App() {
     setCounter((prev) => prev+1)
   }
 
-
   return (
     <div className="App">
       <h1>mars</h1>
       <p>counter value: {counter}</p>
       <button onClick={handleCounterClick}>increase</button>
       <button onClick={handleSendClick}>send</button>
+      <div>
+        <h2>favorite list</h2>
+        <ul className={Styles.list}>
+          {users.map((user) => {
+            return (
+              <li key={user.userId} className={Styles.listItem}>
+                <img className={Styles.avatar} src={user.avatar} alt="" />
+                <div className={Styles.info}>
+                  <p className={Styles.username}>name: {user.username}</p>
+                  <p className={Styles.email}>email: {user.email}</p>
+                </div>
+              </li>
+            )
+          })}
+      </ul>
+      </div>
     </div>
   );
 }
